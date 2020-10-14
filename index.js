86b123e1b8eddf403c755f671e885f4fcb54b7ad
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2020-10-14 22:46:37 +0200

    more robust api with error handling (#2)

--- index.js
+++ index.js
@@ -27,31 +27,62 @@ SOFTWARE.
 const puppeteer = require('puppeteer')
 
 /*
- * @param {string} userName: (mandatory) exact GitHub user name
+ * @param {string} userName: (mandatory) exact GitHub user name; case sensitive!
  * @param {string} country: (mandatory) your home country https://commits.top/
- * @return {string} rank: your rank on commits.top in Contributions category per your home country
+ * @return {string} rank: your rank on commits.top in Commits category per your home country
+ * @return {string} rankContrib: your rank on commits.top in Contributions category per your home country
+ * @return {string} rankAll: your rank on commits.top in All (private, public) commits category per your home country
  */
 
 const getMyRank = async (userName, country) => {
   try {
-    const commitsTopPage = `https://commits.top/${country.toLowerCase().replace(' ', '_')}_public.html`
+    const cmTopPage = `https://commits.top/${country.toLowerCase().replace(' ', '_')}.html`
+    const cmTopPageContrib = `https://commits.top/${country.toLowerCase().replace(' ', '_')}_public.html`
+    const cmTopPageAll = `https://commits.top/${country.toLowerCase().replace(' ', '_')}_private.html`
     const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
     const page = await browser.newPage()
-    await page.goto(commitsTopPage)
-    let rank
-    for (let i = 1; i < 257; i++) {
-      rank = await page.$eval(`tr:nth-child(${i})`, el => el.innerText)
-      if (rank.includes(userName)) {
-        ;[rank] = rank.match(/[0-9]+/)
-        break
+    const pageContrib = await browser.newPage()
+    const pageAll = await browser.newPage()
+    const visit = async (url, pge) => {
+      // abort all images, source: https://github.com/GoogleChrome/puppeteer/blob/master/examples/block-images.js
+      await pge.setRequestInterception(true)
+      pge.on('request', request => {
+        if (request.resourceType() === 'image') {
+          request.abort()
+        } else {
+          request.continue()
+        }
+      })
+      await pge.goto(url, { waitUntil: 'domcontentloaded' })
+      let rank
+      // top 256 GitHub users present
+      for (let i = 1; i < 257; i++) {
+        rank = await pge.$eval(`tr:nth-child(${i})`, el => el.innerText)
+        if (rank.includes(userName)) {
+          ;[rank] = rank.match(/[0-9]+/)
+          break
+        } else if (i === 256) {
+          rank = null
+        }
       }
+      return rank
     }
-    await browser.close()
-    return {
-      rank: rank,
+
+    const [rankCm, rankContrib, rankAll] = await Promise.all([
+      visit(cmTopPage, page),
+      visit(cmTopPageContrib, pageContrib),
+      visit(cmTopPageAll, pageAll)
+    ])
+
+    const rankObj = {
+      rank: rankCm,
+      rankContrib: rankContrib,
+      rankAll: rankAll,
       user: userName,
       country: country
     }
+    await browser.close()
+    return rankObj
   } catch (e) {
     console.error(e)
   }
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 const getMyRank = require('./index')
 
 !(async () => {
+  console.time('time')
   const rankObj = await getMyRank('theDavidBarton', 'Hungary')
-  console.log(rankObj.rank)
+  console.timeEnd('time')
+  console.log(rankObj)
 })()
